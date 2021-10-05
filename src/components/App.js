7ab041d13a52510d1d35e71034906e3f4ec46c72
rhkt <git190811@gmail.com>
2021-10-06 07:12:06 +0900

    setup context

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React,{useReducer} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers';
-import Events from './Events';
-import EventForm from './EventForm';
+import React, { useReducer } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
+import Events from "./Events";
+import AppContext from "../contexts/AppContext";
+import EventForm from "./EventForm";
 
+console.log(AppContext);
 const App = () => {
 	const [state, dispatch] = useReducer(reducer, []);
 
 	return (
 		<>
-			<div className="container-fluid">
-				<EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
-			</div>
+			<AppContext.Provider value="HALLO CONTEXT PROVIDER CHANGE">
+				<div className="container-fluid">
+					<EventForm state={state} dispatch={dispatch} />
+					<Events state={state} dispatch={dispatch} />
+				</div>
+			</AppContext.Provider>
 		</>
 	);
 };
